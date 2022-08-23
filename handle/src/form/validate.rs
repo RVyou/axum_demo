ad162ea3ee34050e000c252067eae8483119b22a
rvyou <1234560>
2022-08-23 16:13:18 +0800

    feat: 完善部分代码

--- handle/src/form/validate.rs
+++ handle/src/form/validate.rs
@@ -1,16 +1,23 @@
 use async_trait::async_trait;
 use axum::{
-    extract::{FromRequest, RequestParts},
+    extract::{Form,FromRequest, RequestParts},
     http::{StatusCode,header},
     response::{IntoResponse, Response},
-    BoxError, Json,
+    BoxError, Json, 
 };
 use serde::de::DeserializeOwned;
 use serde_json::json;
 use thiserror::Error;
 use validator::Validate;
+
+//暂时只弄2个验证类 form 和json
 #[derive(Debug, Clone, Copy, Default)]
 pub struct ValidatedJson<T>(pub T);
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct ValidatedFrom<T>(pub T);
+
+
 
 #[async_trait]
 impl<T, B> FromRequest<B> for ValidatedJson<T>
@@ -30,14 +37,41 @@ where
     }
 }
 
+
+#[async_trait]
+impl<T, B> FromRequest<B> for ValidatedFrom<T>
+where
+    T: DeserializeOwned + Validate,
+    B: http_body::Body + Send,
+    B::Data: Send,
+    B::Error: Into<BoxError>,
+{
+    type Rejection = ServerError;
+
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
+        let Form(value) = Form::<T>::from_request(req).await?;
+        value.validate()?;
+
+        Ok(ValidatedFrom(value))
+    }
+}
+
+
+
 #[derive(Debug, Error)]
 pub enum ServerError {
     #[error(transparent)]
-    ValidationError(#[from] validator::ValidationErrors),
+    ValidationError(#[from] validator::ValidationErrors),//validate 验证错误
+
 
     #[error(transparent)]
-    AxumFormRejection(#[from] axum::extract::rejection::JsonRejection),
+    AxumJsonRejection(#[from] axum::extract::rejection::JsonRejection),//axum json 验证错误
+
+    #[error(transparent)]
+    AxumFormRejection(#[from] axum::extract::rejection::FormRejection),//axum from 验证错误
 }
+
+//实现 IntoResponse 输出错误
 impl IntoResponse for ServerError {
     //处理返回json错误信息
     fn into_response(self) -> Response {
@@ -49,6 +83,15 @@ impl IntoResponse for ServerError {
                 });
                 (StatusCode::BAD_REQUEST ,  [(header::CONTENT_TYPE, "application/json")], message.to_string())
             }
+            ServerError::AxumJsonRejection(_) => (
+                StatusCode::BAD_REQUEST,
+                [(header::CONTENT_TYPE, "application/json")],
+                json!({
+                    "code": 400,
+                    "err":  self.to_string()
+                })
+                .to_string(),
+            ),
             ServerError::AxumFormRejection(_) => (
                 StatusCode::BAD_REQUEST,
                 [(header::CONTENT_TYPE, "application/json")],
@@ -62,3 +105,5 @@ impl IntoResponse for ServerError {
         .into_response()
     }
 }
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,39 @@
 use axum::{
-    handler::Handler,
     body::{Body, Bytes},
+    extract::MatchedPath,
+    handler::Handler,
     http::{Request, StatusCode},
     middleware::{self, Next},
     response::{IntoResponse, Response},
     routing::{get, put},
     Router,
 }; //extract::{Extension},
-use tokio::signal;
-
 use handle::controller::*;
+use tokio::signal;
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+fn log_test() {
+    tracing::debug!("debug  ok");
+    tracing::info!("info  ok");
+    tracing::warn!("warn  ok");
+    tracing::trace!("trace  ok");
+    tracing::error!("error  ok");
+}
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::DEBUG.into()) //日志打印级别控制 none>err>warn>info>debug>trace 小于的包含大于的
+                .parse_lossy(""),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    log_test();
+
     // let _poll = POOL.get_connet(); //lazy_static 运行到代码时才进行初始化
     let app = Router::new()
         .route(
@@ -23,11 +44,11 @@ async fn main() {
         // .route("/test/auth_header_token", delete(route::users_teams_show))//.layer(Extension(pool)))//共享变量 必须实现copy 或者 clone
         // .route("/test/jwt", delete(route::users_teams_show))
         // .route("/test/file", post(route::users_teams_show))
-        // .route("/test/form", delete(route::users_teams_show))
-        .route("/test/json", put(test::Test::json_data))//json 接收并用验证器验证(错误返回json错误) 返回json数据
-        .layer(middleware::from_fn(print_request_response));
+        // .route("/test/form", delete(route::users_teams_show))//form
+        .route("/test/json", put(test::Test::json_data)) //json 接收并用验证器验证(错误返回json错误) 返回json数据
+        .layer(middleware::from_fn(print_message)); //通用打印
 
-    let app = app.fallback(handler_404.into_service());//使用了一个Handler的trait
+    let app = app.fallback(handler_404.into_service()); //使用了一个Handler的trait
 
     axum::Server::bind(&"0.0.0.0:4000".parse().unwrap())
         .serve(app.into_make_service())
@@ -63,41 +84,24 @@ async fn shutdown_signal() {
     println!("优雅关闭 end");
 }
 
-async fn print_request_response(
+async fn print_message(
     req: Request<Body>,
     next: Next<Body>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let (parts, body) = req.into_parts();
-    let bytes = buffer_and_print("request", body).await?;
-    let req = Request::from_parts(parts, Body::from(bytes));
+    let path = if let Some(path) = req.extensions().get::<MatchedPath>() {
+        path.as_str()
+    } else {
+        req.uri().path()
+    };
+    let method = req.method().as_str();
+
+    //所有权 req 被转移了，要复制一份字符串
+    let method = String::from(method);
+    let path = String::from(path);
 
     let res = next.run(req).await;
 
-    let (parts, body) = res.into_parts();
-    let bytes = buffer_and_print("response", body).await?;
-    let res = Response::from_parts(parts, Body::from(bytes));
+    tracing::info!("{} {}", method, path);
 
     Ok(res)
-}
-
-async fn buffer_and_print<B>(direction: &str, body: B) -> Result<Bytes, (StatusCode, String)>
-where
-    B: axum::body::HttpBody<Data = Bytes>,
-    B::Error: std::fmt::Display,
-{
-    let bytes = match hyper::body::to_bytes(body).await {
-        Ok(bytes) => bytes,
-        Err(err) => {
-            return Err((
-                StatusCode::BAD_REQUEST,
-                format!("failed to read {} body: {}", direction, err),
-            ));
-        }
-    };
-
-    if let Ok(body) = std::str::from_utf8(&bytes) {
-        tracing::debug!("{} body = {:?}", direction, body);
-    }
-
-    Ok(bytes)
 }
